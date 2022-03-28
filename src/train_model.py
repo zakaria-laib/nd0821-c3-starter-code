d35c9f1968d31eb5bf2ead72fc4fcc35bdfa260e
@z.laib <z.laib@instadeep.com>
2022-03-28 18:02:43 +0100

    update

--- src/train_model.py
+++ src/train_model.py
@@ -13,8 +13,7 @@ from ml.model import compute_model_slice_metrics
 
 # Add code to load in the data.
 
-print("Data ingestion step")
-data = pd.read_csv("data/census_clean.csv")
+
 
 # Optional enhancement, use K-fold cross validation instead of a
 # train-test split.
@@ -29,53 +28,57 @@ cat_features = [
     "sex",
     "native-country",
 ]
-print("Data segregation step")
-train, test = train_test_split(data, test_size=0.3)
+if __name__ == '__main__':
 
-print("Data preprocessing step for train set")
-X_train, y_train, encoder, lb = process_data(
+    print("Data ingestion step")
+    data = pd.read_csv("data/census_clean.csv")
+    print("Data segregation step")
+    train, test = train_test_split(data, test_size=0.3)
+
+    print("Data preprocessing step for train set")
+    X_train, y_train, encoder, lb = process_data(
     train,
     categorical_features=cat_features,
     label="salary",
     training=True
-)
+    )
 
-# Proces the test data with the process_data function.
-print("Data preprocessing step for test set")
-X_test, y_test, encoder, lb = process_data(
+    # Proces the test data with the process_data function.
+    print("Data preprocessing step for test set")
+    X_test, y_test, encoder, lb = process_data(
     test,
     categorical_features=cat_features,
     label="salary",
     training=False,
     encoder=encoder,
     lb=lb
-)
+    )
 
-# Train and save a model.
-print("Train model step")
-model = train_model(X_train, y_train)
+    # Train and save a model.
+    print("Train model step")
+    model = train_model(X_train, y_train)
 
-print("Inference step")
-preds = inference(model, X_test)
+    print("Inference step")
+    preds = inference(model, X_test)
 
-print("Scoring step")
-precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    print("Scoring step")
+    precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
-print(f"precision: {precision}")
-print(f"recall: {recall}")
-print(f"fbeta: {fbeta}")
+    print(f"precision: {precision}")
+    print(f"recall: {recall}")
+    print(f"fbeta: {fbeta}")
 
-print("Save model")
-joblib.dump(model, "model/model.pkl")
+    print("Save model")
+    joblib.dump(model, "model/model.pkl")
 
-print("Save one-hot encoder")
-joblib.dump(encoder, "model/encoder.pkl")
+    print("Save one-hot encoder")
+    joblib.dump(encoder, "model/encoder.pkl")
 
-print("Save label encoder")
-joblib.dump(lb, "model/lb.pkl")
+    print("Save label encoder")
+    joblib.dump(lb, "model/lb.pkl")
 
-# Computing performance on model slices
-print("Compute model slice metrics")
-input_pth = "model"
-output_pth = "data"
-compute_model_slice_metrics(test, cat_features, input_pth, output_pth, process_data)
+    # Computing performance on model slices
+    print("Compute model slice metrics")
+    input_pth = "model"
+    output_pth = "data"
+    compute_model_slice_metrics(test, cat_features, input_pth, output_pth, process_data)
